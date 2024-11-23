1cbe9e379413959281bf7aa7bc41271ff9cae113
João Serpa <joao.serpapinto@gmail.com>
2024-11-23 17:30:48 +0000

    fix: add conventional-commits ruleset

--- terraform/github/rules.tf
+++ terraform/github/rules.tf
@@ -1,3 +1,4 @@
+# Infrastructure
 resource "github_repository_ruleset" "infrastructure_protect_default_branch" {
   name        = "protect-default-branch"
   repository  = github_repository.infrastructure.name
@@ -34,6 +35,30 @@ resource "github_repository_ruleset" "infrastructure_protect_default_branch" {
   }
 }
 
+resource "github_repository_ruleset" "infrastructure_conventional_commits" {
+  name        = "conventional-commits"
+  repository  = github_repository.infrastructure.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["refs/heads/**"] # Applies to all branches
+      exclude = []
+    }
+  }
+
+  rules {
+    commit_message_pattern {
+      name     = "Conventional Commits"
+      pattern  = "^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\\([\\w\\-\\.]+\\))?(!)?: ([\\w ])+(([\\s\\S])*)"
+      negate   = false
+      operator = "starts_with"
+    }
+  }
+}
+
+# Frontend
 resource "github_repository_ruleset" "frontend_require_branch_protection" {
   name        = "require-branch-protection"
   repository  = github_repository.frontend.name
@@ -70,6 +95,31 @@ resource "github_repository_ruleset" "frontend_require_branch_protection" {
   }
 }
 
+resource "github_repository_ruleset" "frontend_conventional_commits" {
+  name        = "conventional-commits"
+  repository  = github_repository.frontend.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["refs/heads/**"] # Applies to all branches
+      exclude = []
+    }
+  }
+
+  rules {
+    commit_message_pattern {
+      name     = "Conventional Commits"
+      pattern  = "^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\\([\\w\\-\\.]+\\))?(!)?: ([\\w ])+(([\\s\\S])*)"
+      negate   = false
+      operator = "starts_with"
+    }
+  }
+}
+
+
+# Backend
 resource "github_repository_ruleset" "backend_require_branch_protection" {
   name        = "require-branch-protection"
   repository  = github_repository.backend.name
@@ -102,6 +152,29 @@ resource "github_repository_ruleset" "backend_require_branch_protection" {
       require_last_push_approval        = false
       required_approving_review_count   = 1
       required_review_thread_resolution = false
+    }
+  }
+}
+
+resource "github_repository_ruleset" "backend_conventional_commits" {
+  name        = "conventional-commits"
+  repository  = github_repository.backend.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["refs/heads/**"] # Applies to all branches
+      exclude = []
+    }
+  }
+
+  rules {
+    commit_message_pattern {
+      name     = "Conventional Commits"
+      pattern  = "^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\\([\\w\\-\\.]+\\))?(!)?: ([\\w ])+(([\\s\\S])*)"
+      negate   = false
+      operator = "starts_with"
     }
   }
 }
