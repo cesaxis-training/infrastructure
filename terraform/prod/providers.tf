542cc7bbe1c4519ae02dfe1b3c1d475cbff61602
filipeazvd <99filipe99@gmail.com>
2024-11-25 12:59:24 +0000

    more changes

--- terraform/prod/providers.tf
+++ terraform/prod/providers.tf
@@ -1,11 +1,22 @@
 terraform {
   required_providers {
     digitalocean = {
+<<<<<<< HEAD
       source = "digitalocean/digitalocean"
+=======
+      source  = "digitalocean/digitalocean"
+>>>>>>> a4b6f47 (more changes)
       version = "~> 2.0"
     }
   }
 }
 
+<<<<<<< HEAD
 provider "digitalocean" {
 }
+=======
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  
+}
+>>>>>>> a4b6f47 (more changes)
